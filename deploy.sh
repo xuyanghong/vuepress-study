c36a871f8994f2bafc8a7974742185275d759cb9
xuyanghong <790619181@qq.com>
2021-08-08 21:25:18 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# 终止一个错误
+set -e
+
+# 生成静态资源
+yarn docs:build
+# 进入生成的构建文件夹
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:xuyanghong/xuyanghong.github.io.git master
+
+cd -
